783f91f9731c0b64d31bb97e628dd15d1d075aa8
Aleksandr <alatar325@gmail.com>
2017-10-27 00:15:01 +0200

    Refactor slightly, remove geerlingguy.homebrew

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 xcode-select --install || true
-pip install ansible
-ansible-galaxy install -r requirements.yml
-ansible-playbook main.yml -i inventory -K
+
+# Yeah, not the best installation method... 
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install ansible
 
